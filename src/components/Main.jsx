9394ff8b5bb3e55d2a4c792cd52c698a78d29462
Paweł Sobolewski <pawelsobolewski@amnerbit.com>
2018-03-09 22:29:53 +0100

    Simple form validation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,53 +1,94 @@
 import React from 'react';
 import Holder from 'holderjs'
-import { Form, FormGroup, Label, Input, FormFeedback, FormText, Col, Container, Media} from 'reactstrap';
+import { Form, FormGroup, Label, Input, FormFeedback, FormText, Col, Container, Media, Row} from 'reactstrap';
 
 export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      name: '',
+      age: '',
+      avatar: ''
+    };
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleAgeChange = this.handleAgeChange.bind(this);
+    
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleEmailChange(event) {
+    this.setState({email: event.target.value});
+    let emailValid = event.target.value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i) ? false:true;
+    this.setState({validEmail: emailValid});
+  }
+  
+  handleNameChange(event) {
+    this.setState({name: event.target.value});
+    let nameValid = event.target.value.length > 3 ? false:true;
+    this.setState({validName: nameValid});
+  }
+  
+  handleAgeChange(event) {
+    this.setState({age: event.target.value});
+    let ageValid = (isFinite(String(event.target.value)) && event.target.value < 105) ? false:true;
+    this.setState({validAge: ageValid});
+  }
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <Form>
         <Container>
-          <FormGroup row>
-            <Col sm="12" md={{ size: 4, offset: 2 }}>
-              <Media row>
-                <Media left href="#">
-                  <Media object data-src="holder.js/256x256" alt="Generic placeholder image" />
+          <FormGroup>
+            <Row>
+              <Col sm="12" md={{ size: 4, offset: 2 }}>
+                <Media>
+                  <Media left href="#">
+                    <Media object data-src="holder.js/256x256" alt="Generic placeholder image" />
+                  </Media>
                 </Media>
-              </Media>
-            </Col>
-            <Col sm="12" md={{ size: 4 }}>
-              <Label for="exampleFile">Avatar</Label>
-              <Input type="file" name="file" id="exampleFile" />
-              <FormText color="muted">
-                Pleace upload imge file for the avatar.
-              </FormText>
-            </Col>
+              </Col>
+              <Col sm="12" md={{ size: 4 }}>
+                <Label for="exampleFile">Avatar</Label>
+                <Input type="file" name="file" id="exampleFile" />
+                <FormText color="muted">
+                  Pleace upload imge file for the avatar.
+                </FormText>
+              </Col>
+            </Row>
           </FormGroup>
-          <FormGroup row>
+          <FormGroup>
             <Col sm="12" md={{ size: 8, offset: 2 }}>
-              <Label for="exampleEmail">Email</Label>
-              <Input />
-              <FormFeedback>You will not be able to see this</FormFeedback>
-              <FormText>Example help text that remains unchanged.</FormText>
+              <Label for="email">Email</Label>
+              <FormText>Please provide vaild email address.</FormText>
+              <Input value={this.state.email}  onChange={this.handleEmailChange} valid={this.state.validEmail} invalid={this.state.validEmail} />
+              <FormFeedback>Oh noes! Invalid email format.</FormFeedback>
             </Col>
           </FormGroup>
-          <FormGroup row>
+          <FormGroup>
             <Col sm="12" md={{ size: 8, offset: 2 }}>
-              <Label for="exampleEmail">Valid input</Label>
-              <Input valid />
-              <FormFeedback valid>Sweet! that name is available</FormFeedback>
-              <FormText>Example help text that remains unchanged.</FormText>
+              <Label for="name">Full name</Label>
+              <FormText>Please provide your full name(min length 3 chars).</FormText>
+              <Input value={this.state.name}  onChange={this.handleNameChange} valid={this.state.validName} invalid={this.state.validName} />
+              <FormFeedback>Name is too short.</FormFeedback>
             </Col>
           </FormGroup>
-          <FormGroup row>
+          <FormGroup>
             <Col sm="12" md={{ size: 8, offset: 2 }}>
-              <Label for="examplePassword">Invalid input</Label>
-              <Input invalid />
-              <FormFeedback>Oh noes! that name is already taken</FormFeedback>
-              <FormText>Example help text that remains unchanged.</FormText>
+              <Label for="age">Your age</Label>
+              <FormText>Please provide your age(numeric value only).</FormText>
+              <Input value={this.state.age}  onChange={this.handleAgeChange} valid={this.state.validAge} invalid={this.state.validAge} />
+              <FormFeedback>Age must be typical numeric value for age(less than 105 - you can't live that long).</FormFeedback>
+              <FormFeedback>Or can you......</FormFeedback>
             </Col>
           </FormGroup>
-        </Container>
+      </Container>
       </Form>
     );
   }
